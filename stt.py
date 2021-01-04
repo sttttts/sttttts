4143b5ca17848dae9c5790f00fabcea81d8dd82f
Gamererr <starkid2u@outlook.com>
2021-01-04 18:23:07 -0500

    ctrl+h to start stt

--- stt.py
+++ stt.py
@@ -1,14 +1,25 @@
 import speech_recognition as sr
+from system_hotkey import SystemHotkey
 
-r = sr.Recognizer()
+# VVVVVV stt part; the heavy lifter of this file VVVVVV
 
-while True:
-    with sr.Microphone() as source:
+r = sr.Recognizer() # adding object for the recognizer
+
+def listen():
+    with sr.Microphone() as source: # making the system default mic as the input source
         print("Say Anything:")
-        audio = r.listen(source=source)
+        audio = r.listen(source=source) # listening to the input
 
         try:
-            text = r.recognize_google(audio)
+            text = r.recognize_google(audio) # understanding the input
             print(f'you said "{text}"')
         except:
             print("lol i didnt know what you said")
+
+
+
+
+# VVVVVV the part that activates the stt VVVVVV
+
+hk = SystemHotkey()
+hk.register(('control', 'shift', 'h'), callback=lambda x:listen())
