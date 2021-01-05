1c935c44b69e89d8dba7c85d307d160a1ad3e3a3
Gamererr <starkid2u@outlook.com>
2021-01-04 22:12:52 -0500

    fixed up install file

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe" -OutFile "python-3.9.1-amd64.exe"
 .\python-3.9.1-amd64.exe /passive Include_pip=1 Include_launcher=1 Include_exe=1
-python -m pip install pip PySide2 pipwin SpeechRecognition win32con pyttsx3
+py -m pip install pip PySide2 pipwin SpeechRecognition pyttsx3 system_hotkey
 pipwin install PyAudio
