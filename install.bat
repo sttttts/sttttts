4edcab71299b196d6a3455b1de8d990bde8c62aa
Gamererr <starkid2u@outlook.com>
2021-01-12 22:21:41 -0500

    update install files

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-py -m pip install pip PySide2 pipwin SpeechRecognition pyttsx3 system_hotkey
+py -m pip install pip PySimpleGUI pipwin SpeechRecognition pyttsx3 system_hotkey
 pipwin install PyAudio
