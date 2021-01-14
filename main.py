5aa6c7a74a6595bd406f4aacd197bee535e7772a
Vresod <lukebinkofsky@gmail.com>
2021-01-14 08:41:01 -0500

    Add volume/sensitivity sliders

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 # Define the window's contents
 layout = [
-	[sg.Text("What's your name?")],
-	[sg.Input(key='-INPUT-')],
-	[sg.Text(size=(40,1), key='-OUTPUT-')],
+	[sg.Text("Volume %")],
+	[sg.Slider(range = (0, 100),orientation="h",default_value=70)],
+	[sg.Text("Sensitivity %")],
+	[sg.Slider(range = (0, 100),orientation="h",default_value=80)],
 	[sg.Button('Ok'), sg.Button('Quit')]
 ]
 
 # Create the window
-window = sg.Window('Window Title', layout)
+window = sg.Window('sttttts', layout)
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -25,8 +26,6 @@ while True:
 	# See if user wants to quit or window was closed
 	if event == sg.WINDOW_CLOSED or event == 'Quit':
 		break
-	# Output a message to the window
-	window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
 
 # Finish up by removing from the screen
 window.close()
