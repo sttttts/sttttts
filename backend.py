83f681e56492142f163f5441a2f609819a89822d
Vresod <lukebinkofsky@gmail.com>
2021-02-01 09:21:31 -0500

    turn most of backend.py into a class i did 0 testing on this btw

--- backend.py
+++ backend.py
@@ -6,6 +6,7 @@ import speech_recognition as sr
 
 r = sr.Recognizer() # adding object for the recognizer
 engine = pyttsx3.init()
+mic = sr.Microphone()
 
 def get_io_devices():
 	p = pyaudio.PyAudio()
@@ -21,56 +22,62 @@ def get_io_devices():
 			out.append(p.get_device_info_by_host_api_device_index(0, i).get('name'))
 	return inp,out
 
-def stt(inp):
+class sttttts:
+	"""
+	Defines the class that does speech to text, to text to speech.
+	"""
+	def __init__(self,input_index,output_index):
+		self.mic = sr.Microphone(device_index=input_index)
+		self.output_index = output_index
+	def stt(self):
 
-	# speech to text
-	if inp: pass
-	with sr.Microphone(device_index=inp) as source: # making the system default mic as the input source
-		print("Say Anything:")
-		audio = r.listen(source=source) # listening to the input
-		print(f"Stopped Listenning")
-		try:
-			text = r.recognize_google(audio) # understanding the input
-			print(f'you said "{text}"')
-		except:
-			print("lol i didnt know what you said")
-			return
-		del(source)
-		return text
+		# speech to text
+		with self.mic as source: # making the system default mic as the input source
+			print("Say Anything:")
+			audio = r.listen(source=source) # listening to the input
+			print(f"Stopped Listenning")
+			try:
+				text = r.recognize_google(audio) # understanding the input
+				print(f'you said "{text}"')
+			except:
+				print("lol i didnt know what you said")
+				return
+			return text
 
-def make_stt_file(text):
-	# text to speech
-	
-	engine.save_to_file(text, 'tts.wav')
-	engine.runAndWait()
+	def make_stt_file(self,text):
+		# text to speech
 
-def tts(out):
-	# playing to VB-Audio Cable Input
+		engine.save_to_file(text, 'tts.wav')
+		engine.runAndWait()
 
-	wf = wave.open("tts.wav", 'rb')
-	p = pyaudio.PyAudio()
-	CHUNK = 182
-	discord = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-		channels=wf.getnchannels(),
-		rate=wf.getframerate(),
-		output=True,
-		output_device_index=out)
+	def tts(self):
+		# playing to VB-Audio Cable Input
 
-	speakers = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-		channels=wf.getnchannels(),
-		rate=wf.getframerate(),
-		output=True)
+		wf = wave.open("tts.wav", 'rb')
+		p = pyaudio.PyAudio()
+		CHUNK = 182
+		discord = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+			channels=wf.getnchannels(),
+			rate=wf.getframerate(),
+			output=True,
+			output_device_index=self.output_index)
 
-	data = wf.readframes(CHUNK)
+		speakers = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+			channels=wf.getnchannels(),
+			rate=wf.getframerate(),
+			output=True)
 
-	while data != b'':
-		discord.write(data)
-		speakers.write(data)
 		data = wf.readframes(CHUNK)
 
+		while data != b'':
+			discord.write(data)
+			speakers.write(data)
+			data = wf.readframes(CHUNK)
+
 def main(inp,out):
-	make_stt_file(stt(inp))
-	tts(out)
+	io = sttttts(inp,out)
+	io.make_stt_file(io.stt())
+	io.tts()
 
 if __name__ == "__main__":
 	main(0,0)
