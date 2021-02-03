fa5ab9d9878d35398ce8ef1b75f00dea4aa3f7bd
Vresod <lukebinkofsky@gmail.com>
2021-02-03 14:42:16 -0500

    remove install scripts, they were cringe

--- install.bat
+++
@@ -1,2 +0,0 @@
-py -m pip install pip PySimpleGUI pipwin SpeechRecognition pyttsx3 system_hotkey
-pipwin install PyAudio
--- install.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe" -OutFile "python-3.9.1-amd64.exe"
-.\python-3.9.1-amd64.exe /passive Include_pip=1 Include_launcher=1 Include_exe=1
-py -m pip install pip PySimpleGUI pipwin SpeechRecognition pyttsx3 system_hotkey
-pipwin install PyAudio
