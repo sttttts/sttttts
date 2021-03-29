509474f76834ca92c70af317c1780a7a5317b4cd
Vresod <lukebinkofsky@gmail.com>
2021-03-29 15:57:18 -0400

    fix imports and move __main__.py and main.pyw to symlinks

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # This Python file uses the following encoding: utf-8
-import sys
-import os
 import ctypes
 from system_hotkey import SystemHotkey
 import PySimpleGUI as sg
-import backend
+import backend as b
 
 myappid = 'sttttts.sttttts.sttttts.1' # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
@@ -17,11 +15,11 @@ layout = [
 	# [sg.Text("Sensitivity %")],
 	# [sg.Slider(range = (0, 100),orientation="h",default_value=80,key="-SENSITIVTY-")],
 	[sg.Text("Input")],
-	[sg.Combo(backend.get_io_devices()[0],readonly=True,default_value=backend.get_io_devices()[0][0],key="-INPUT-")],
+	[sg.Combo(b.get_io_devices()[0],readonly=True,default_value=b.get_io_devices()[0][0],key="-INPUT-")],
 	[sg.Text("Output")],
-	[sg.Combo(backend.get_io_devices()[1],readonly=True,default_value=backend.get_io_devices()[1][0],key="-OUTPUT-")],
+	[sg.Combo(b.get_io_devices()[1],readonly=True,default_value=b.get_io_devices()[1][0],key="-OUTPUT-")],
 	[sg.Text("Output 2")],
-	[sg.Combo(["Disabled"] + backend.get_io_devices()[1],readonly=True,default_value="Disabled",key="-OUTPUT2-")],
+	[sg.Combo(["Disabled"] + b.get_io_devices()[1],readonly=True,default_value="Disabled",key="-OUTPUT2-")],
 	[sg.Text("Say manually")],
 	[sg.Input(key="-MANUAL-"),sg.Button("Say")],
 	[sg.Button('Ok'), sg.Button('Quit')]
@@ -29,8 +27,8 @@ layout = [
 # Create the window
 window = sg.Window('sttttts', layout,icon="logos\\icon.ico")
 hk = SystemHotkey()
-hk.register(('control', 'q'), callback=lambda x:backend.main(None,None,None))
-hk.register(('alt', 'q'), callback=lambda x:backend.repeat(None,None))
+hk.register(('control', 'q'), callback=lambda x:b.main(None,None,None))
+hk.register(('alt', 'q'), callback=lambda x:b.repeat(None,None))
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -38,7 +36,7 @@ while True:
 	# See if user wants to quit or window was closed
 	if event == sg.WINDOW_CLOSED or event == 'Quit':
 		break
-	devices = backend.get_io_devices()
+	devices = b.get_io_devices()
 	wanted_input = devices[0].index(values["-INPUT-"])
 	wanted_output = devices[1].index(values["-OUTPUT-"]) + len(devices[0])
 	if values["-OUTPUT2-"] == "Disabled":
@@ -46,10 +44,10 @@ while True:
 	else:
 		wanted_output2 = devices[1].index(values["-OUTPUT2-"]) + len(devices[0])
 	if event == 'Say':
-		backend.say(wanted_output,wanted_output2,values["-MANUAL-"])
+		b.say(wanted_output,wanted_output2,values["-MANUAL-"])
 	hk.unregister(('control', 'q'))
 	hk.unregister(('alt', 'q'))
-	hk.register(('control', 'q'), callback=lambda x:backend.main(wanted_input,wanted_output,wanted_output2))
-	hk.register(('alt', 'q'), callback=lambda x:backend.repeat(wanted_output,wanted_output2))
+	hk.register(('control', 'q'), callback=lambda x:b.main(wanted_input,wanted_output,wanted_output2))
+	hk.register(('alt', 'q'), callback=lambda x:b.repeat(wanted_output,wanted_output2))
 # Finish up by removing from the screen
 window.close()
